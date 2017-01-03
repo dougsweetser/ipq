39ac2bc59785787970ba2e04cbfc23217dfcccb7
Doug Sweetser <sweetser@alum.mit.edu>
2017-01-03 14:26:20 -0500

    Finished first draft.

--- q_notebook/billiard_calculations.py
+++ q_notebook/billiard_calculations.py
@@ -1,19 +1,26 @@
 
 # coding: utf-8
 
-# # Billiards With Space-time Numbers
+# # Table of Contents
+# * [Observing Billiards Using Space-time Numbers](#Observing-Billiards-Using-Space-time-Numbers)
+# 	* [Representations of Numbers  Versus Coordinate Transformation of Vectors](#Representations-of-Numbers--Versus-Coordinate-Transformation-of-Vectors)
+# 	* [Observer B Boosted](#Observer-B-Boosted)
+# 	* [Observer C in a Gravity Field in Theory](#Observer-C-in-a-Gravity-Field-in-Theory)
+# 	* [Observer C in a Gravity Field in Practice](#Observer-C-in-a-Gravity-Field-in-Practice)
+# 	* [Conclusions](#Conclusions)
+# 
 
-# The goal of this iPython notebook is to become familiar with using space-time numbers to describe events. This will be done for three different observers. The first case will cover the fact that the observers happen to be at different locations. How does one handle different ways to represent the numbers used to characterize events?
+# # Observing Billiards Using Space-time Numbers
 
-# One observer will be set in constant motion. We will work out the equivalence classes that cover observers in motion. The final case will look at equivalence classes that may happen due to gravity.
+# The goal of this iPython notebook is to become familiar with using space-time numbers to describe events. This will be done for three different observers. The first case will cover the fact that the observers happen to be at different locations. How does one handle different ways to represent the numbers used to characterize events? One observer will be set in constant motion. We will work out the equivalence classes that cover observers in motion. The final case will look at equivalence classes that may happen due to gravity.
 
 # Here is an animation of a mini billiard shot.
 
 # ![](images/Billiards/billiard_video.gif)
 
-# Cue ball hits the 8 ball, then into the corner pocket it goes. Observer A is yellow, our proverbial reference observer. I promise to do nothing with her ever. Observer B in pink is at a slightly different location, but still watching from the tabletop. Eventually, he will be set into constant motion. Then we can see about what Observers agree and disagree about. Observer C is in purple and at the end of a pipe cleaner above the tabletop. His observations will be ever-so-slightly different from Observer A and that will be investigated.
+# The cue ball hits the 8 ball, then into the corner pocket it goes. Observer A is yellow, our proverbial reference observer. I promise to do nothing with her ever. Observer B in pink is at a slightly different location, but still watching from the tabletop. Eventually, he will be set into constant motion. We can see about what Observers agree and disagree about. Observer C is in purple and at the end of a pipe cleaner above the tabletop. His observations will be ever-so-slightly different from Observer A due to the effects of gravity and that will be investigated woff.
 
-# A number of big simplifications will be done for this analysis. All but two frames will be used.
+# A number of simplifications will be done for this analysis. All but two frames will be used.
 
 # ![](images/Billiards/shot_12.png)
 
@@ -27,7 +34,7 @@
 
 # Can the time be measured precisely? In this case, I will use the frames of the gif animation as a proxy for measuring time. I used the command "convert billiard_video.gif Frames/billiards_1%02d.png" to make make individual frames from the gif. The two frames are 147 and 158. The speed of the fastest cue break is over 30 miles per hour, or as a diminsionless relativistic speed is 4.5x10<sup>-8</sup>. If small number are used for the differences in space, then the difference between time should be scaled to be in the ten billion range. So that is what I did: call the first time 1,470,000,000 and the second one 1,580,000,000. The ball is then moving around 20mph. I could have found out the frames per second, and calculated the correct speed from there. The three observers do not need to coordinate to figure out the same origin in time, so I chose B and C to start a billion and two billion earlier respectively.
 
-# This explains how I got numbers related to an 8 ball moving on a table. Now to calculate the in
+# This explains how I got numbers related to an 8 ball moving on a table. Now to start calculating with the goal of getting the square. I have written a test of tools called "Q_tool_devo" that allow for numerical manipulations of something I call "space-time numbers". Essentially they are quaternions, a 4D division algebra, written in a funny way. Instead of writing a real number like 5.0, a doublet of values is used, say (6.0, 1.0) which can then be "reduced" to (5.0, 0) and is thus equivalent to the standard real number 5.0. To create a space-time number, feed it eight numbers like so:
 
 # In[1]:
 
@@ -43,23 +50,29 @@ print(Aq1s)
 print(Aq2s)
 
 
-# When scaled, the expected values are seen, the x value at around 2.5, the y value above 3 and zero for z.
+# When scaled, the expected values are seen, the x value at around 2.5, the y value above 3 and zero for z. Event 2 is 9.5 and 0.8 $j_3$ meaning in real numbers, -0.8. There is also the qtype "QxS", a way of keeping track of what operations have been done to a space-time number. After all, all space-time numbers look the same. Keeping the qtype around help avoid combining differing qtypes.
 
 # Calculate the delta quaternion between events one and two:
 
-# In[3]:
+# In[37]:
 
 Adq=Aq2s.dif(Aq1s).reduce()
+print(Aq2s.dif(Aq1s))
 print(Adq)
 
 
-# The difference is nearly 7 in the x<sub>1</sub> direction, and 4 in the j<sub>3</sub>, which if real numbers were being used would be the positive x and negative y. The qtype "QxQ-QxQ.reduce" shows that both initial components were multiplied by a scalar value, then the difference taken.
+# The difference is nearly 7 in the x<sub>1</sub> direction, and 4 in the j<sub>3</sub>, which if real numbers were being used would be the positive x and negative y. The qtype "QxQ-QxQ.reduce" shows that both initial components were multiplied by a scalar value, the difference taken, then reduced to its real number equivalent form.
 
-# In[4]:
+# Distances are found using a square.
+
+# In[38]:
 
 Adq2=Adq.square()
 print(Adq2)
+print(Adq2.reduce())
 
+
+# This is a case where the non-reduced form is more convenient. The time squared is about 60 quadrillion while the change in space squared is slighly over 64. Classical physics is full of such imbalances and the non-reduced form helps maintian the separation.
 
 # It is my thesis that all the numbers in the square provide important information for comparing any pair of observers. Here are the input numbers for observers B and C:
 
@@ -177,7 +190,7 @@ print(vx)
 # 
 # Boost the delta by this velocity.
 
-# In[17]:
+# In[12]:
 
 Bdq_boosted = Bdq.boost(beta_x = vx)
 print(Bdq_boosted)
@@ -190,7 +203,7 @@ print(Bdq_boosted.dif(Bdq).reduce())
 
 # Compare the squares of the boosted with the non-boosted Observer B
 
-# In[18]:
+# In[13]:
 
 print(Bdq_boosted.square())
 print(Bdq.square())
@@ -198,7 +211,7 @@ print(Bdq.square())
 
 # Time and space are mixing together for the boosted frame. There are two huge numbers for $I_0$ and $I_2$ instead of a big number and about 65. Are they the same? Compare the reduced squares:
 
-# In[19]:
+# In[14]:
 
 print(Bdq_boosted.square().reduce())
 print(Bdq.square().reduce())
@@ -206,6 +219,148 @@ print(Bdq.square().reduce())
 
 # The reduced intervals are the same. The space-times-time terms are not. The difference between the space-times-time terms can be used to determine how Observer B boosted in moving relative to Observer B (calculation not done here). Even with out going into detail, the motion is only along x because that is the only term that changes.
 
-# ## Observer C in a Gravity Field
+# Software was written to systematically look at equivalences classes for a pair of quaternions. Three types of comparisons are made: linear, squared, and the norm.
 
-# We know from the video of the billiard balls that they are in a gravity field since the eight ball drops into the pocket.
+# In[15]:
+
+qb = qtd.EQ(Bdq, Bdq_boosted)
+print(qb)
+
+
+# There are 9 equivalences classes in all. Let's visualize them a set of icons
+
+# In[16]:
+
+qb.visualize()
+
+
+# The figures in gray are location in time and 3 for space. The colorful set with parabolas are the squares, the interval being purple and off-yellow, space-times-time in green. The norm is in pink.
+
+# For the gray set, the events from Observer B are being compared with a boosted Observer B for motion that is only along the $x$ direction. We thus expec the $y$ and $z$ values to be exact as they are (down exact and here exact because $z=0$). The value of $x$ is boosted, so they are both right, but not the same value. But what about time? The report is for an exact match. The software was written to say two values were equivalent if they were the same to 10 significant digits. It is the 16th significant digit which is different.
+
+# The time-like interval is the same for Observer B and the boosted one, so the equivalence class is timelike-exact as expected. This graphics are icons to represent the class, not a reflection of the numbers used. The space-times-time terms are only different along $t x$ due to the boost along $x$.
+
+# The norm is included for completeness of simple operartions, but I don't understand it at this time. It is marked as exact due to the dominance of the time term.
+
+# ## Observer C in a Gravity Field in Theory
+
+# The video of the billiard balls shows there is a gravity field since the eight ball drops into the pocket. Newton's law of gravity can be written as an interval:
+
+# $$d \tau^2 = \left(1 - 2\frac{G M}{c^2 R}\right) dt^2 - dR^2/c^2 $$
+
+# More precise measurements of weak field gravity adds a few more terms (essentiall eq. 40.1 of Misner, Thorne and Wheeler):
+
+# $$d \tau^2 = \left(1 - 2\frac{G M}{c^2 R} + 2 \left(\frac{G M}{c^2 R}\right)^2\right) dt^2 - \left(1 + 2\frac{G M}{c^2 R}\right) dR^2 /c^2 $$
+
+# When the mass $M$ goes to zero or the distance from the source gets large, the result is the interval expected in flat space-time.
+
+# The space-times-times equivalence class as gravity proposal stipulates that for a simple gravitational source mass (spherically symmetric, non-rotating, uncharged) the square of a delta quaternion produces a space-times-time that is the same for different observers no matter where they are in a gravitational field. This can be achieved by making the factor for time be the inverse of the one for space (below, a dimensionless M is a standin for $\frac{G M}{c^2 R}$).
+
+# In[17]:
+
+from sympy import symbols
+M = symbols('M')
+(1/(1 - 2 * M)).series(M, 0, n=2)
+
+
+# Even in the "classical realm", the space-times-time equivalence class as gravity proposal is different from Newtonian gravity. From my brief study of the rotation of thin disk galaxies, this term is not applied to such calculations. This now strikes me as odd. The Schwarzschild solution has this same term, the "first order in M/R", yet only the dt correction is used in practice. The rotation profile calculation is quite complex, needing eliptical integrals. An analytic solution like that would be altered by this well know term. It will be interesting in time to explore if the extra term has consequences.
+
+# Since we are analyzing the square, the delta quaternion would be the square root of with these two terms that use the dimensionless gravitational length:
+
+# $$ \begin{align*} dq &= \left(\sqrt{1 - 2 \frac{G M}{c^2 R}} dt, \sqrt{1 + 2 \frac{G M}{c^2 R}} dR/c \right) \\ dq^2 &= \left( \left(1 - 2 \frac{G M}{c^2 R}\right) dt^2 - \left(1 + 2 \frac{G M}{c^2 R}\right) dR^2/c^2, 2 ~dt ~dR/c  \right) \\
+# &= \left( d\tau^2, 2 ~dt ~dR/c \right) \end{align*} $$
+
+# To be consistent with the weak gravity field tests and the algebraic constraints of the equivalence class proposal requires six terms not five:
+
+# In[18]:
+
+(1/(1 - 2 * M + 2 * M ** 2)).series(M, 0, n=3)
+
+
+# Here are the delta quaternion and its square in a gravity field that will be consistent with all weak field gravitational tests.
+
+# $$ \begin{align*} dq &= \left(\sqrt{1 - 2 \frac{G M}{c^2 R} + 2 \left(\frac{G M}{c^2 R}\right)^2} dt, \sqrt{1 + 2 \frac{G M}{c^2 R} + 2 \left(\frac{G M}{c^2 R}\right)^2} dR/c \right) \\ dq^2 &= \left( \left(1 - 2 \frac{G M}{c^2 R} + 2 \left(\frac{G M}{c^2 R}\right)^2\right) dt^2 - \left(1 + 2 \frac{G M}{c^2 R} + 2 \left(\frac{G M}{c^2 R}\right)^2\right) dR^2/c^2, 2 ~dt ~dR/c  \right) \\
+# &= \left( d\tau^2, 2 ~dt ~dR/c \right) \end{align*} $$
+
+# The second order term for $ dR^2 $ has consequences that are tricky to discuss. Notice that no mention has been made of metric, not field equations, nor covariant and contravariant vectors. That is because numbers are tensors of rank 0 that are equiped with rules of multiplication and division. As discussed above, there are different representations of numbers like a Cartesian representation, a cylindrical representation, and a spherical representation. My default is to use the Cartesian representation because I find it simplest to manage.
+
+# The most successful theory for gravity, general relativity, does using metrics, covariant and contravariant tensors, as well as connections that reveal how a metric changes in space-time. There are a great many technical choices in this process which have consequences. Einstein worked with a torsion-free connection that was metric compatible. One consequence is that dealing with fermions is an open puzzle. The process of getting to an interval is not simple. Twenty non-linear equation equations must be solved. This can be done analytically for only the simplest of cases. It is such a case, the Schwarzschild solution, that makes up most of the tests of general relativity (eq. 40.1 from MTW written above in isotrophic coordinates).
+
+# The reader is being asked to compare Einstein's apple of an interval to the first of four oranges. There is no overlap between the mechanics of the math, hence the apple versus orange. The forms of the expressions are the same: a Taylor series in a dimensionless gravitational length. Five of the coefficients of the Taylor series are identical. Those five coefficients have been tested in a wide variety of classical tests of weak gravitational fields.
+
+# The sixth term is not the same for the Taylor series expansion of the Schwarzschild solution in either isotrophic or Schwarzschild coordinates. It is not resonable to expect the simple space-times-time equivalence constraint will solve the non-linear Einstein field equations.
+
+# The truncated series expansion will not be the final story. We could wait for experimentalist to determine 10 terms, but that is quite unresonable (Personal story: I spend ~$600 to go to an Eastern Gravity Meeting just to ask Prof. Clifford Will when we might get the terms for second order Parameterize Post-Newtonian accuracy, and at the time he knew of no such planned experimental effort ~2005?). Given that gravity is o harmonic phenomena, there are six terms that match, and many other people have made the same speculation, it is a small leap to suggest that a postive and negative exponential to the dimensionless mass length may be the complete solution for simple systems:
+
+# $$ \begin{align*} dq &= \left(\exp\left({-\frac{G M}{c^2 R}}\right) dt, \exp\left(\frac{G M}{c^2 R} \right) dR/c \right) \\ dq^2 &= \left( \exp \left(-2\frac{G M}{c^2 R} \right) dt^2 - \exp \left( 2 \frac{G M}{c^2 R} \right) dR^2/c^2, 2 ~dt ~dR/c  \right) \\
+# &= \left( d\tau^2, 2 ~dt ~dR/c \right) \end{align*} $$
+
+# The exponential interval does appear in the literature since it makes calculations far simpler. 
+
+# ## Observer C in a Gravity Field in Practice
+
+# Gravity is impressively weak. The distance of Observer C over Observer A is impressively small. The change in the interval should in practice be beyond measure.
+
+# \begin{align*}
+# G&=6.67384\cdot 10^{-11} \frac{m^{3}}{kg s^2}\\
+# M&=5.9722 \cdot 10^{24} kg\\
+# c&=299792458 m / s \\
+# R&=6.371 \cdot 10^{6} m
+# \end{align*}
+
+# In[22]:
+
+GMc2R_for_Observer_A = 6.67384e-11 * 5.9722e+24 / (299792458 ** 2 * 6371000)
+GMc2R_for_Observer_C = 6.67384e-11 * 5.9722e+24 / (299792458 ** 2 * 6371000.1)
+print(GMc2R_for_Observer_A)
+print(GMc2R_for_Observer_C)
+
+
+# Moving 10 centimeters is not much.
+
+# Do the "minimal" shift meaning the three terms of the Taylor series.
+
+# In[29]:
+
+Adq_g = Cdq.g_shift(GMc2R_for_Observer_A, g_form="minimal")
+Cdq_g = Cdq.g_shift(GMc2R_for_Observer_C, g_form="minimal")
+print(Adq_g)
+print(Cdq_g)
+
+
+# The squares could be calculated, but if the imput values are the same, there will be no difference in any of the squares. This is consistent with expectations: a small number change in a small number cannot be noticed.
+
+# Observer C is a mere 10 centimeters away from Observer A. Let us make the distance so vast that the GMc2R value is zero.
+
+# In[34]:
+
+Cdq_g_zero = Cdq.g_shift(0, g_form="minimal")
+print(Adq_g)
+print(Cdq_g_zero)
+
+
+# Get far enough away, and the effects of gravity may become apparent.
+
+# In[33]:
+
+Adq_g_2 = Adq_g.square() 
+Cdq_g_zero_2 = Cdq_g_zero.square()
+eq_g = qtd.EQ(Adq_g_2, Cdq_g_zero_2)
+print(eq_g)
+eq_g.visualize()
+
+
+# The time-like interval is not exact. That is the effect of gravity. Yet these pairs of observers, A and C at an very great distance from A, all have exact space-times-time values as the quaternion gravity proposal demands.
+
+# ## Conclusions
+
+# Nearly all calculations in physics I have ever done involved vector spaces over a mathematical field like real or complex numbers. This iPython notebook used a fundamentally new and different toolset. Space-time numbers can always be made equivalent to the 4D division algebra of quaternions through the process of reduction. This new breed of numbers was applied to the simple case of an eightball moving along a pool table. Threee observers watched the events unfold. Their raw data always was different because that is inherent in the definition of being a different observer. Yet by taking the difference between two events and squaring it, equivalence classes could be defined. If two observers agree on the first term of the square, that means the two observers are moving at a constant speed relative to each other. The other three terms can be used to figure out the motion. While this might be view as a novel approach to special relativity, nothing new is claimed.
+
+# If two observers are in an equivlance class because the difference between two events squared space-times-time values are exactly the same, this is a new proposal for how gravity works. There is no metric tensor, connection, nor field equations. Rather, the proposal is a twin of special relativity. In both cases it is the square that matters. The difference between the two is if the observers agree to the reduced real values, that is special relativity, where if they agree to the reducded imaginary values, that is space-times-time equivalence class as gravity.
+
+# Space-time numbers should feel odd. We expect numbers to be unique. Yet Nature plays unusual games with the numbers for particles such as boson versus fermion statistics. Space-time numbers may be rich enough to reflect these kinds of properties. This notebook represents a proof of concept.
+
+# In[ ]:
+
+
+
